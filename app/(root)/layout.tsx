db72c03024f2eb634ae3e867108ca3f1eb16727c
phamvu.tinh <phamvu.tinh99@gmail.com>
2023-10-01 16:43:41 +0700

    [ADD] Navbar + Setting Layout

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,27 @@
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import prismadb from "@/lib/prismadb";
+
+type Props = {
+	children: React.ReactNode;
+};
+
+const SetupLayout = async ({ children }: Props) => {
+	const { userId } = auth();
+	if (!userId) {
+		redirect("/sign-in");
+	}
+	const store = await prismadb.store.findFirst({
+		where: {
+			userId,
+		},
+	});
+
+	if (store) {
+		redirect(`/${store.id}`);
+	}
+	return <>{children}</>;
+};
+
+export default SetupLayout;
